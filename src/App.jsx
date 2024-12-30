615982b01ea29da5e5b3b661f6d38561067d2e74
yeowun05-12 <taking0n@naver.com>
2024-12-31 00:33:06 +0900

    명언 api 연결 완료

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
   return (
     <div className='container'>
       <CurrentTime />
+      <LifeQuotes />
       <Header />
       <InputValue addTodo={addTodo} />
       <TodoList todos={todos} setTodos={setTodos} />
@@ -158,6 +159,54 @@ const TodoList = ({ todos, setTodos }) => {
         ))}
       </ul>
     </>
+  );
+};
+
+// 명언 컨포넌트
+const LifeQuotes = () => {
+  const [lifeQuotes, setLifeQuotes] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      //try 블록은 에러를 처리하기 위해 사용이 된다. try 안에서 에러가 발생하면 catch 블록으로 넘겨져서 에러를 처리할 수 있게 된다.
+      try {
+        //fetch 는 기본적으로 비동기 함수여서 네트워크 요청을 기다리는 동안 코드가 멈추지 않고 계속 실행 되지만,
+        // await를 사용하면 결과를 받기 전까지 코드의 실행을 잠시 멈춘다.
+        const response = await fetch(
+          'https://korean-advice-open-api.vercel.app/api/advice'
+        );
+        if (response.ok) {
+          const data = await response.json(); // JSON 형태로 파싱
+          console.log(data);
+          setLifeQuotes(data);
+          setLoading(false);
+        } else {
+          throw new Error('명언을 불러오는데 실패했습니다.');
+        }
+      } catch (err) {
+        //if else 처럼 괄호 뒤에 바로 붙어야함.
+        setError(err.message);
+        setLoading(false); // 로딩 끝
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  if (loading) {
+    return <div>로딩 중 ...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+  return (
+    <div className='life_quotes'>
+      <p className='content'>{lifeQuotes.message}</p>
+      <p className='name'>{lifeQuotes.author}</p>
+    </div>
   );
 };
 
