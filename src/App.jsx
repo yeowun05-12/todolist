cb33b205510d1a4b4cfa56508864fc2f13f5b293
yeowun05-12 <taking0n@naver.com>
2024-12-30 23:50:35 +0900

    CRUD 구현 완료 / 현재 시간 표시 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 0, content: '아침먹기', time: new Date() },
-    { id: 1, content: '모던 자바스크립트 책읽기', time: new Date() },
-    { id: 2, content: '영어 공부하기', time: new Date() },
+    { id: 0, content: '아침먹기', isCompleted: false },
+    {
+      id: 1,
+      content: '모던 자바스크립트 책읽기',
+      time: new Date(),
+      isCompleted: false,
+    },
+    { id: 2, content: '영어 공부하기', isCompleted: false },
   ]);
 
   const addTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
   };
 
+  // 현재 시간
+
   return (
-    <>
+    <div className='container'>
+      <CurrentTime />
       <Header />
       <InputValue addTodo={addTodo} />
-      <TodoList todos={todos} />
-    </>
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
   );
 }
 
@@ -29,6 +37,30 @@ const Header = () => {
   );
 };
 
+// 시간을 표시하기 위한 컨포넌트
+
+const CurrentTime = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); //1초마다 갱신이 됨
+
+    // 컨포넌트 언마운트 시 interval을 정리
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <p> 현재 시간 : {currentTime.toLocaleTimeString()}</p>
+      </div>
+    </>
+  );
+};
+
+// 인풋에 밸류값들어오면 추가할 수 있게 만든 로직
 const InputValue = ({ addTodo }) => {
   const [inputValue, setInputValue] = useState('');
 
@@ -58,17 +90,69 @@ const InputValue = ({ addTodo }) => {
   );
 };
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
+  const [editedId, setEditedId] = useState(null);
+
+  // 삭제 하는 함수 로직
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  //완료 체크 하는 로직
+  const toggleCompletion = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
   return (
     <>
       <ul>
         {todos.map((todo, index) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button onClick={() => {}}>수정</button>
-            <button>삭제</button>
+          <li
+            key={todo.id}
+            style={{
+              textDecoration: todo.isCompleted ? 'line-through' : 'none',
+            }}
+          >
+            {editedId === todo.id ? (
+              <>
+                <input
+                  type='text'
+                  defaultValue={todo.content}
+                  onChange={(e) => {
+                    todo.content = e.target.value;
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    setEditedId(null);
+                  }}
+                >
+                  저장
+                </button>
+              </>
+            ) : (
+              <>
+                {todo.content}
+                <button
+                  onClick={() => {
+                    setEditedId(todo.id);
+                  }}
+                >
+                  수정
+                </button>
+              </>
+            )}
+            <button onClick={() => handleDelete(todo.id)}>삭제</button>
             <label>
-              <input type='checkBox' /> 완료
+              <input
+                type='checkBox'
+                checked={todo.isCompleted}
+                onChange={() => toggleCompletion(todo.id)}
+              />
+              완료
             </label>
           </li>
         ))}
